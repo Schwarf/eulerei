fddfbf56c52a207e7961e322f51536d7e9ee2f89
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-08-21 16:42:27 +0200

    Add Problem 40 solution.

---
+++ Problem40_Champrnowne_Constant/main.cpp
@@ -0,0 +1,24 @@
+//
+// Created by andreas on 21.08.22.
+//
+#include <string>
+#include <iostream>
+
+int main()
+{
+	std::string help;
+	help.reserve(1000001);
+	for(int i=1; i < 300000; ++i)
+	{
+		help += std::to_string(i);
+	}
+	std::cout << help.size() << std::endl;
+	int result = std::stoi(help.substr(0,1 )) *
+		std::stoi(help.substr(9,1 )) *
+		std::stoi(help.substr(99,1 )) *
+		std::stoi(help.substr(999,1 )) *
+		std::stoi(help.substr(9999,1 )) *
+		std::stoi(help.substr(99999,1 )) *
+		std::stoi(help.substr(999999,1 ));
+	std::cout << result << std::endl;
+}
