a5f81f6c4b857b2165283378a495c3189628dd91
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-08-22 17:03:31 +0200

    Solve problem 53.

--- Problem53_Combinatoric_Selections/main.cpp
+++ Problem53_Combinatoric_Selections/main.cpp
@@ -1,3 +1,37 @@
 //
 // Created by andreas on 22.08.22.
 //
+#include <vector>
+#include <iostream>
+int main()
+{
+	std::vector<std::vector<long long unsigned >> result(101, std::vector<long long unsigned>(101, 0));
+	result[0][0] = 0;
+	result[1][0] = 1;
+	result[1][1] = 1;
+	int count{};
+	for(int i=2; i < 101; ++i)
+	{
+		for(int j=0; j <= i; ++j)
+		{
+			if(j==0)
+				result[i][j] = 1;
+			else if(i==j)
+				result[i][j] = 1;
+			else if(i-1 == j)
+				result[i][j] = i;
+			else if (j > i)
+				result[i][j] = 0;
+			else
+				result[i][j] = result[i-1][j-1] + result[i-1][j];
+
+			if(result[i][j] > 1000000)
+				count++;
+		}
+	}
+	std::cout << count << std::endl;
+
+
+	return 0;
+}
+
