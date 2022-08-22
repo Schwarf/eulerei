eaf8ef1509a9a6c10888a64c7c45adab6d3b49ee
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-08-22 17:04:02 +0200

    Add helper function to read CSV files.

--- helpers/read_csv_file.h
+++ helpers/read_csv_file.h
@@ -4,5 +4,31 @@
 
 #ifndef READ_CSV_FILE_H
 #define READ_CSV_FILE_H
+#include <vector>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+std::vector<std::vector<std::string>> read_csv_file(std::string full_path_to_file)
+{
+	std::fstream file(full_path_to_file, std::ios::in);
+	std::vector<std::vector<std::string>> file_content;
+	std::string line;
+	std::vector<std::string> row;
+	std::string word;
+	if(!file.is_open())
+		std::cout << "Could not open file" << std::endl;
+
+	while(std::getline(file, line));
+	{
+		row.clear();
+		std::stringstream stream(line);
+		while(std::getline(stream, word, ','))
+			row.push_back(word);
+		file_content.push_back(row);
+	}
+	return file_content;
+}
 
 #endif //READ_CSV_FILE_H
