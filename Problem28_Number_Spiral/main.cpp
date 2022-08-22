1b3a70926d2b67cdddffba095b2510e1bec636b9
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-08-22 17:03:05 +0200

    Solve problem 28

--- Problem28_Number_Spiral/main.cpp
+++ Problem28_Number_Spiral/main.cpp
@@ -1,3 +1,21 @@
 //
 // Created by andreas on 22.08.22.
 //
+
+#include <iostream>
+// n = (dimension -1)/2
+int answer(int n)
+{
+	n = (n-1)/2;
+	//auto upper_right_quadrant = 4 * n * n + 4 * n + 1;
+	//auto lower_left_quadrant = 4 * n * n + 1;
+	//auto lower_right_quadrant = 4 * n * n - 2 * n + 1;
+	//auto upper_left_quadrant = 4 * n * n + 2 * n + 1;
+	//Sum above formulas from 1..n  (add 1 for center value)
+	return 2 * n * (8 * n * n + 15 * n + 13) / 3 + 1;
+}
+
+int main()
+{
+	std::cout << answer(1001) << std::endl;
+}
--- Problem40_Champrnowne_Constant/main.cpp
+++ Problem40_Champrnowne_Constant/main.cpp
@@ -3,22 +3,21 @@
 //
 #include <string>
 #include <iostream>
-
+#include "./../helpers/get_all_divisors.h"
 int main()
 {
 	std::string help;
 	help.reserve(1000001);
-	for(int i=1; i < 300000; ++i)
-	{
+	for (int i = 1; i < 300000; ++i) {
 		help += std::to_string(i);
 	}
 	std::cout << help.size() << std::endl;
-	int result = std::stoi(help.substr(0,1 )) *
-		std::stoi(help.substr(9,1 )) *
-		std::stoi(help.substr(99,1 )) *
-		std::stoi(help.substr(999,1 )) *
-		std::stoi(help.substr(9999,1 )) *
-		std::stoi(help.substr(99999,1 )) *
-		std::stoi(help.substr(999999,1 ));
+	int result = std::stoi(help.substr(0, 1)) *
+		std::stoi(help.substr(9, 1)) *
+		std::stoi(help.substr(99, 1)) *
+		std::stoi(help.substr(999, 1)) *
+		std::stoi(help.substr(9999, 1)) *
+		std::stoi(help.substr(99999, 1)) *
+		std::stoi(help.substr(999999, 1));
 	std::cout << result << std::endl;
 }
