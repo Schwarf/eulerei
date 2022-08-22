775320306042e0e14b22d688e1e9c23c91bca152
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-08-22 17:04:38 +0200

    Update CMakeLists.txt. Start with problem 42.

--- Problem42_Coded_Triangle_Numbers/main.cpp
+++ Problem42_Coded_Triangle_Numbers/main.cpp
@@ -1,3 +1,11 @@
 //
 // Created by andreas on 22.08.22.
 //
+#include "./../helpers/read_csv_file.h"
+
+int main()
+{
+	std::string full_file_path("/media/linux_data/projects/cpp/eulerei/Problem42_Coded_Triangle_Numbers/p042_words.txt");
+	auto result = read_csv_file(full_file_path);
+	return 0;
+}
