43f4f2959ea9841ed8ee67a7431074bb859c7645
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-08-22 17:13:48 +0200

    Solve problem 42

--- Problem42_Coded_Triangle_Numbers/main.cpp
+++ Problem42_Coded_Triangle_Numbers/main.cpp
@@ -2,10 +2,29 @@
 // Created by andreas on 22.08.22.
 //
 #include "./../helpers/read_csv_file.h"
-
+#include <unordered_set>
 int main()
 {
 	std::string full_file_path("/media/linux_data/projects/cpp/eulerei/Problem42_Coded_Triangle_Numbers/p042_words.txt");
 	auto result = read_csv_file(full_file_path);
+	std::unordered_set<int> triangle_values;
+	for(int i=1; i < 1000; ++i)
+	{
+		triangle_values.insert(i*(i+1)/2);
+	}
+	int count{};
+	for(const auto & word: result[0])
+	{
+		int sum{};
+		for(const auto & character: word)
+		{
+			sum += 1 + character - 'A' ;
+
+		}
+		if(triangle_values.find(sum) != triangle_values.end())
+			count++;
+	}
+	std::cout << count << std::endl;
+
 	return 0;
 }
