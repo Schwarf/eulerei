2517e74d2491bf1ac487c4402da6a25a7240c881
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-08-24 17:22:08 +0200

    Add sample executable to test is_power_of.

--- helpers/main_test_power_of.cpp
+++ helpers/main_test_power_of.cpp
@@ -1,3 +1,28 @@
 //
 // Created by andreas on 24.08.22.
 //
+
+#include "is_power_of.h"
+#include <iostream>
+int main()
+{
+	int x1=21, x2=12, x3=27;
+	int y1=16, y2=12, y3=27;
+	int z1 = 17*17, z2 = 17*16, z3 = 67890;
+	std::cout << is_power_of<3, int>(x1) << std::endl;
+	std::cout << is_power_of<3, int>(x2) << std::endl;
+	std::cout << is_power_of<3, int>(x3) << std::endl;
+
+	std::cout << is_power_of<4, int>(y1) << std::endl;
+	std::cout << is_power_of<4, int>(y2) << std::endl;
+	std::cout << is_power_of<4, int>(y3) << std::endl;
+
+	for(int i{}; i < 100000000; ++i)
+	{
+		if(is_power_of<17, int>(i))
+			std::cout << i << std::endl;
+	}
+
+	return 0;
+
+}
