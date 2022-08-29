2fc1a520d858900d3922eeea8fcd2d19367f5bba
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-08-29 17:27:12 +0200

    Use helper function.

--- Problem46_GoldbachOtherConjecture/main.cpp
+++ Problem46_GoldbachOtherConjecture/main.cpp
@@ -5,31 +5,7 @@
 #include <unordered_set>
 #include <vector>
 #include <algorithm>
-
-template <typename T>
-void compute_primes_up_to_n(T n, std::unordered_set<T> & primes)
-{
-	std::vector<bool> is_prime(n +1, true);
-	is_prime[0] = false;
-	is_prime[1] = false;
-	for(int64_t i =2; i*i <= n; ++i)
-	{
-		if(is_prime[i])
-		{
-			for(int64_t j = i*i; j <= n; j+=i)
-			{
-				is_prime[j] = false;
-			}
-		}
-	}
-	for(int i=0; i <= n; ++i)
-	{
-		if(is_prime[i])
-			primes.insert(i);
-	}
-
-}
-
+#include "./../helpers/compute_primes.h"
 
 
 
