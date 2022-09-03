ab913b4cb3363a3836d71ea7ef4d7fa181708404
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-09-03 12:30:47 +0200

    Work on std::string to std::bitset conversion.

--- helpers/big_integer.h
+++ helpers/big_integer.h
@@ -10,53 +10,96 @@
 #include <utility>
 #include <algorithm>
 #include <iostream>
+#include <vector>
 
-template <size_t bit_size>
+template<size_t bit_size>
 class BigInteger
 {
 public:
 
-	BigInteger(long long initial_value){
-		if(initial_value < 0) {
+	BigInteger(long long initial_value)
+	{
+		if (initial_value < 0) {
 			initial_value = -initial_value;
 			is_negative_ = true;
 		}
-		value_ = std::bitset<bit_size>(initial_value);
+		absolute_value_ = std::bitset<bit_size>(initial_value);
 	}
-	BigInteger(const std::string & initial_value)
+	BigInteger(const std::string &initial_value)
 	{
-		value_ = convert_string_to_bitset(initial_value);
+		absolute_value_ = convert_string_to_bitset(initial_value);
 	}
 
-
-
-private:
-	std::bitset<bit_size> convert_string_to_bitset(const std::string str)
+	BigInteger(const std::bitset<bit_size> &initial_absolute_value, bool is_negative)
+		:
+		absolute_value_(initial_absolute_value),
+		is_negative_(is_negative)
 	{
-		validate_string(str);
-
 	}
-	void validate_string(const std::string & str)
+
+	std::string validate_string_and_determine_sign(const std::string &str)
 	{
-		if(str.empty()) {
-			std::cerr << "String is empty" << std::endl;
+		if (str.empty()) {
+			std::cerr << "BigInteger: String is empty" << std::endl;
 		}
 		auto sign = str[0];
-		if(!std::isdigit(sign) && sign != '-') {
-			std::cerr << "First character in string is neither number nor 'minus' sign but " << sign << std::endl;
+		if (!std::isdigit(sign) && sign != '-') {
+			std::cerr << "BigInteger: First character in string is neither number nor 'minus' sign but: " << str
+					  << std::endl;
 		}
 		int first_digit_position{};
-		if(sign == '-') {
+		if (sign == '-') {
+			if (str.size() < 2)
+				std::cerr << "BigInteger: String contains only a minus sign: " << str << std::endl;
 			is_negative_ = true;
-			first_digit_position =1;
+			first_digit_position = 1;
 		}
-		if(!std::all_of(str.cbegin()+first_digit_position, str.cend(), [](char c) { return std::isdigit(c); })) {
-			std::cerr << "String contains non-numeric values!!!!" << std::endl;
+		if (str[first_digit_position] == 0) {
+			std::cerr << "BigInteger: Leading zero in string detected: " << str << std::endl;
+		}
+		if (!std::all_of(str.cbegin() + first_digit_position, str.cend(), [](char c)
+		{ return std::isdigit(c); })) {
+			std::cerr << "BigInteger: String contains non-numeric values: " << str << std::endl;
+		}
+		return str.substr(first_digit_position);
+	}
+
+private:
+	std::bitset<bit_size> convert_string_to_bitset(std::string str)
+	{
+
+		auto absolute_value_string = validate_string_and_determine_sign(str);
+		int digit{};
+		int remainder{};
+		int bit_index{};
+		int last_character_index = absolute_value_string.size() - 1;
+		while (last_character_index > -1) {
+			digit = remainder + absolute_value_string[last_character_index--] - '0' ;
+			absolute_value_[bit_index++] = digits_to_bits_[digit];
+
 		}
 
 	}
+	std::string divide_string_by_2(const std::string & input)
+	{
+		int carryover{};
+		int adder {};
+		std::string result;
+		int digit {};
+		for(const auto & character: input)
+		{
+			digit = character -'0';
+			carryover = digit /2 + adder;
+			adder = digits_to_bits_[digit]*5;
+			result += '0'+carryover;
+		}
+		result.erase(0, result.find_first_not_of('0'));
+		return result;
+	}
+
 	bool is_negative_{false};
-	std::bitset<bit_size> value_;
+	std::bitset<bit_size> absolute_value_;
+	std::vector<int> digits_to_bits_{0, 1, 0, 1, 0, 1, 0, 1, 0, 1};
 };
 
 
--- helpers/main_test_power_of.cpp
+++ helpers/main_test_power_of.cpp
@@ -4,6 +4,7 @@
 
 #include "is_power_of.h"
 #include <iostream>
+#include <bitset>
 int main()
 {
 	int x1=21, x2=12, x3=27;
@@ -17,11 +18,24 @@ int main()
 	std::cout << is_power_of<4, int>(y2) << std::endl;
 	std::cout << is_power_of<4, int>(y3) << std::endl;
 
-	for(int i{}; i < 100000000; ++i)
+	for(int i{}; i < 1000; ++i)
 	{
 		if(is_power_of<17, int>(i))
 			std::cout << i << std::endl;
 	}
+	std::string h{"17"};
+	std::bitset<10> result;
+	int digit{};
+	int remainder{};
+	int bit_index{};
+	int last_character_index = h.size() - 1;
+	while (last_character_index > -1) {
+		digit = h[last_character_index--] - '0' + remainder;
+		result[bit_index++] =  digit % 2;
+		remainder = digit/2;
+	}
+	std::cout << result.to_ulong() << std::endl;
+
 
 	return 0;
 
