c3c986e457418c72cbbf9cb78a19f9249138c5e3
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-09-03 19:58:04 +0200

    Simple addition but only for positive numbers.

--- helpers/big_integer.h
+++ helpers/big_integer.h
@@ -41,17 +41,17 @@ public:
 	{
 	}
 	// Copy constructor
-	BigInteger(const BigInteger<number_of_bits> & other)
-	:
-	absolute_value_(other.absolute_value_),
-	is_negative_(other.is_negative_)
+	BigInteger(const BigInteger<number_of_bits> &other)
+		:
+		absolute_value_(other.absolute_value_),
+		is_negative_(other.is_negative_)
 	{
 	}
 
 	// Assignment operators
-	BigInteger & operator=(const BigInteger<number_of_bits> & other)
+	BigInteger &operator=(const BigInteger<number_of_bits> &other)
 	{
-		if(this == other)
+		if (this == other)
 			return *this;
 		absolute_value_ = other.absolute_value_;
 		is_negative_ = other.is_negative_;
@@ -60,9 +60,15 @@ public:
 	}
 
 	// Equal operator
-	bool operator==(const BigInteger<number_of_bits>& rhs)
+	bool operator==(const BigInteger<number_of_bits> &rhs)
 	{
 		return are_equal(*this, rhs);
+	}
+
+	BigInteger<number_of_bits> &operator+(const BigInteger<number_of_bits> &rhs)
+	{
+		BigInteger<number_of_bits> result;
+		addition(this->absolute_value_, rhs.absolute_value_);
 	}
 
 	std::string validate_string_and_determine_sign(const std::string &str)
@@ -93,29 +99,41 @@ public:
 	}
 
 
-
 private:
 
-	bool are_equal(const BigInteger<number_of_bits> & left_hand_side, const BigInteger<number_of_bits> & right_hand_side)
+	void addition(std::bitset<number_of_bits> b1, std::bitset<number_of_bits> b2,
+				  std::bitset<number_of_bits> &result)
 	{
-		if(left_hand_side.is_negative_ != right_hand_side.is_negative_)
+		std::bitset<number_of_bits> carry_over;
+		while (b1.any()) {
+			carry_over = b1;
+			carry_over &= b2;
+			carry_over <<= 1;
+			b1 ^= b2;
+			b2 = std::move(carry_over);
+		}
+		result = std::move(b1);
+	}
+
+	bool are_equal(const BigInteger<number_of_bits> &left_hand_side, const BigInteger<number_of_bits> &right_hand_side)
+	{
+		if (left_hand_side.is_negative_ != right_hand_side.is_negative_)
 			return false;
-		for(int i=0; i < number_of_bits; ++i)
-		{
-			if(right_hand_side.absolute_value_[i] != left_hand_side.absolute_value_[i])
+		for (int i = 0; i < number_of_bits; ++i) {
+			if (right_hand_side.absolute_value_[i] != left_hand_side.absolute_value_[i])
 				return false;
 		}
 		return true;
 	}
 
-	std::bitset<number_of_bits> convert_string_to_bitset(const std::string & str)
+	std::bitset<number_of_bits> convert_string_to_bitset(const std::string &str)
 	{
 		auto absolute_value_string = validate_string_and_determine_sign(str);
 		int digit{};
 		int bit_index{};
 		std::bitset<number_of_bits> result;
 		while (!absolute_value_string.empty()) {
-			digit =  absolute_value_string.back() - '0' ;
+			digit = absolute_value_string.back() - '0';
 			// instead of 'modulo 2' operation we just set the bits for odd decimal digits
 			result[bit_index++] = IS_ODD_DECIMAL_DIGIT_[digit];
 			absolute_value_string = divide_string_decimal_number_by_2(absolute_value_string);
@@ -123,22 +141,20 @@ private:
 		return result;
 	}
 
-
-	std::string divide_string_decimal_number_by_2(const std::string & input)
+	std::string divide_string_decimal_number_by_2(const std::string &input)
 	// we divide starting from the most significant (leftmost) digit
 	{
 		std::string result;
 		int quotient{};
 		// if we divide e.g. {10, 30, 50, 70, 90,} we get {5, 15, ... } with a 5 on the following digit.
 		// for {0, 20, 40, ...} this is not the case
-		int carry_over_five_or_zero {};
-		int digit {};
-		for(const auto & character: input)
-		{
-			digit = character -'0';
-			quotient = digit /2 + carry_over_five_or_zero;
+		int carry_over_five_or_zero{};
+		int digit{};
+		for (const auto &character: input) {
+			digit = character - '0';
+			quotient = digit / 2 + carry_over_five_or_zero;
 			carry_over_five_or_zero = IS_ODD_DECIMAL_DIGIT_[digit] ? 5 : 0;
-			result += '0'+quotient;
+			result += '0' + quotient;
 		}
 		result.erase(0, result.find_first_not_of('0'));
 		return result;
