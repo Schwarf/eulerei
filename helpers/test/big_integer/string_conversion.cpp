7c8ad7a90befd416e8d63c6129d21ef69ff18377
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-09-04 19:19:24 +0200

    Finish tests for string conversion.

--- helpers/test/big_integer/string_conversion.cpp
+++ helpers/test/big_integer/string_conversion.cpp
@@ -14,7 +14,8 @@ protected:
 	std::string bad_string_with_leading_zero{"0509"};
 	std::string good_string_negative{"-178"};
 	std::string bad_string_negative_with_leading_zero{"-089378193572438572389478238947435435435"};
-	std::string bad_string_negative_with_letter{"-0572438578230948230948230941"};
+	std::string bad_string_negative_with_letter{"-572438578230948230948230941X"};
+	std::string bad_string_only_minus_sign{"-"};
 };
 
 TEST_F(SetupBigIntegersStringConversion, string_with_letter)
@@ -29,6 +30,20 @@ TEST_F(SetupBigIntegersStringConversion, string_with_letter)
 
 }
 
+TEST_F(SetupBigIntegersStringConversion, string_with_letter_on_first_position)
+{
+	try {
+		auto test = BigInteger<100>(bad_string_with_letter_on_first_position);
+		FAIL() << "Expected std::invalid_argument";
+	}
+	catch (std::invalid_argument const &error) {
+		EXPECT_TRUE(std::regex_match(error.what(),
+									 std::regex(
+										 "BigInteger: First character in string is neither number nor 'minus' sign but: (.*)")));
+	}
+
+}
+
 TEST_F(SetupBigIntegersStringConversion, string_with_symbol)
 {
 	try {
@@ -36,8 +51,58 @@ TEST_F(SetupBigIntegersStringConversion, string_with_symbol)
 		FAIL() << "Expected std::invalid_argument";
 	}
 	catch (std::invalid_argument const &error) {
-		std::cout << error.what() << std::endl;
 		EXPECT_TRUE(std::regex_match(error.what(), std::regex("BigInteger: String contains non-numeric values: (.*)")));
 	}
 
 }
+
+TEST_F(SetupBigIntegersStringConversion, string_with_leading_zero)
+{
+	try {
+		auto test = BigInteger<100>(bad_string_with_leading_zero);
+		FAIL() << "Expected std::invalid_argument";
+	}
+	catch (std::invalid_argument const &error) {
+		EXPECT_TRUE(std::regex_match(error.what(), std::regex("BigInteger: Leading zero in string detected: (.*)")));
+	}
+
+}
+
+
+TEST_F(SetupBigIntegersStringConversion, string_negative_with_letter)
+{
+	try {
+		auto test = BigInteger<100>(bad_string_negative_with_letter);
+		FAIL() << "Expected std::invalid_argument";
+	}
+	catch (std::invalid_argument const &error) {
+		EXPECT_TRUE(std::regex_match(error.what(), std::regex("BigInteger: String contains non-numeric values: (.*)")));
+	}
+
+}
+
+TEST_F(SetupBigIntegersStringConversion, string_negative_with_leading_zero)
+{
+	try {
+		auto test = BigInteger<100>(bad_string_negative_with_leading_zero);
+		FAIL() << "Expected std::invalid_argument";
+	}
+	catch (std::invalid_argument const &error) {
+		EXPECT_TRUE(std::regex_match(error.what(), std::regex("BigInteger: Leading zero in string detected: (.*)")));
+	}
+
+}
+
+TEST_F(SetupBigIntegersStringConversion, string_only_minus_sign)
+{
+	try {
+		auto test = BigInteger<100>(bad_string_only_minus_sign);
+		FAIL() << "Expected std::invalid_argument";
+	}
+	catch (std::invalid_argument const &error) {
+		EXPECT_TRUE(std::regex_match(error.what(), std::regex("BigInteger: String contains only a minus sign: (.*)")));
+	}
+
+}
+
+
