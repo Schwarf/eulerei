f285eff610007e7644b5c1e26ee13073a82dabcb
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-09-05 19:10:22 +0200

    Initial unit tests for subtraction.

--- helpers/test/big_integer/subtraction.cpp
+++ helpers/test/big_integer/subtraction.cpp
@@ -10,8 +10,10 @@ protected:
 	std::string big_number1{"9278118309180938109380983109"};
 	std::string big_number2{"2342349278113243248309180938234234109380983109572387399309213"};
 	std::string big_number3{"99985092384309223098482340923184482309480342341093809831372189479238438579"};
-	std::string big_number1_plus_2{"2342349278113243248309180938234243387499292290510496780292322"};
-	std::string big_number2_plus_3{"99985092384311565447760454166432791490418576575203190814481761866637747792"};
+	std::string big_number1_minus_2{"-2342349278113243248309180938234224831262673928634278018326104"};
+	std::string big_number2_minus_1{"2342349278113243248309180938234224831262673928634278018326104"};
+	std::string big_number2_minus_3{"-99985092384306880749204227679936173128542108106984428848262617091839129366"};
+	std::string big_number3_minus_2{"99985092384306880749204227679936173128542108106984428848262617091839129366"};
 	std::string number1{"17"};
 	std::string number2{"9"};
 	std::string number1_minus_number2{"8"};
@@ -22,8 +24,17 @@ protected:
 
 };
 
+TEST_F(SetupBigIntegersSubtraction, test_subtraction_big_number1_minus_big_number2)
+{
+	auto result = big_int1 - big_int2 ;
+	//std::cout << result.to_binary_string() << std::endl;
+	EXPECT_TRUE(result.to_number_string() == big_number1_minus_2);
+	result = big_int2 - big_int1;
+	EXPECT_TRUE(result.to_number_string() == big_number2_minus_1);
+}
 
-TEST_F(SetupBigIntegersSubtraction, test_addition_number1_minus_number2)
+
+TEST_F(SetupBigIntegersSubtraction, test_subtraction_number1_minus_number2)
 {
 	auto result = BigInteger<10>(number1) - BigInteger<10>(number2) ;
 	EXPECT_TRUE(result.to_number_string() == number1_minus_number2);
@@ -31,3 +42,10 @@ TEST_F(SetupBigIntegersSubtraction, test_addition_number1_minus_number2)
 	EXPECT_TRUE(result.to_number_string() == number2_minus_number1);
 }
 
+TEST_F(SetupBigIntegersSubtraction, test_subtraction_big_number2_minus_big_number3)
+{
+	auto result = big_int2 - big_int3 ;
+	EXPECT_TRUE(result.to_number_string() == big_number2_minus_3);
+	result = big_int3 - big_int2;
+	EXPECT_TRUE(result.to_number_string() == big_number3_minus_2);
+}
