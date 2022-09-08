7c6709ea9e06282d5356b620cbcc6e62b5419c90
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-09-08 20:05:45 +0200

    Add more unit tests for multiplication.

--- helpers/test/big_integer/multiplication.cpp
+++ helpers/test/big_integer/multiplication.cpp
@@ -20,12 +20,35 @@ protected:
 	BigInteger<1000> big_int1 = BigInteger<1000>(big_number1);
 	BigInteger<1000> big_int2 = BigInteger<1000>(big_number2);
 	BigInteger<1000> big_int3 = BigInteger<1000>(big_number3);
-
+	std::string big_int1_times_big_int2
+		{"21732593723759235407399765787370415333957046911250030416004483672888629718439465421083217"};
+	std::string big_int1_times_big_int3
+		{"927673516296006980729894560881502774070244898464224480249709981742727683326165883231534258393132962111"};
+	std::string big_int2_times_big_int3
+		{"234200008968472643897563759490017090900853806121255762546487800286933351961031909024365909260379740965737038749477505295498821329328327"};
 };
 
-TEST_F(SetupBigIntegersMultiplication, test_multiplication_)
+TEST_F(SetupBigIntegersMultiplication, test_multiplication_number1_times_number2)
 {
 	auto result = BigInteger<10>(number1) * BigInteger<10>(number2);
 	EXPECT_TRUE(result.to_number_string() == number1_times_number2);
 }
 
+TEST_F(SetupBigIntegersMultiplication, test_multiplication_big_int1_times_big_int2)
+{
+	auto result = big_int1 * big_int2;
+	EXPECT_TRUE(result.to_number_string() == big_int1_times_big_int2);
+}
+
+TEST_F(SetupBigIntegersMultiplication, test_multiplication_big_int1_times_big_int3)
+{
+	auto result = big_int1 * big_int3;
+	EXPECT_TRUE(result.to_number_string() == big_int1_times_big_int3);
+}
+
+TEST_F(SetupBigIntegersMultiplication, test_multiplication_big_int2_times_big_int3)
+{
+	auto result = big_int2 * big_int3;
+	EXPECT_TRUE(result.to_number_string() == big_int2_times_big_int3);
+
+}
