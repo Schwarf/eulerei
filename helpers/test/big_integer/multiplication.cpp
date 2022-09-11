ba606f24eae599e8aed29438e2761a769d2b5680
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-09-11 14:54:37 +0200

    Add more unit tests for multiplication with negative numbers.

--- helpers/test/big_integer/multiplication.cpp
+++ helpers/test/big_integer/multiplication.cpp
@@ -14,12 +14,17 @@ protected:
     std::string big_number1{"9278118309180938109380983109"};
     std::string big_number2{"2342349278113243248309180938234234109380983109572387399309213"};
     std::string big_number3{"99985092384309223098482340923184482309480342341093809831372189479238438579"};
+    std::string big_number4{"-48391025835790328541839049309"};
+    std::string big_number5{"-84901240928409248392043045678909876345"};
     std::string number1{"17"};
     std::string number2{"9"};
     std::string number1_times_number2{"153"};
     BigInteger<1000> big_int1 = BigInteger<1000>(big_number1);
     BigInteger<1000> big_int2 = BigInteger<1000>(big_number2);
     BigInteger<1000> big_int3 = BigInteger<1000>(big_number3);
+    BigInteger<1000> big_int4 = BigInteger<1000>(big_number4);
+    BigInteger<1000> big_int5 = BigInteger<1000>(big_number5);
+
     BigInteger<250> big_int1_less_bits = BigInteger<250>(big_number1);
     BigInteger<250> big_int2_less_bits = BigInteger<250>(big_number2);
     std::string big_int1_times_big_int2
@@ -28,8 +33,13 @@ protected:
             {"927673516296006980729894560881502774070244898464224480249709981742727683326165883231534258393132962111"};
     std::string big_int2_times_big_int3
             {"234200008968472643897563759490017090900853806121255762546487800286933351961031909024365909260379740965737038749477505295498821329328327"};
+    std::string big_int4_times_big_int5
+            {"4108458143257311198335562726147027215480712648692379030265547695605"};
     std::string big_int1_times_big_int2_times_big_int3
             {"2172935391220725947951388165768892241012727217727475924473900459678405564867005266948939185637595909040828940716851363882143413508995324447139681383396661902228643"};
+    std::string big_int1_times_big_int2_times_big_int4
+            {"-1051662504365167903027407927691094605989616893410712462560445224362040738760182883622729597834189342551085664255347053"};
+
 };
 
 TEST_F(SetupBigIntegersMultiplication, test_multiplication_number1_times_number2) {
@@ -61,6 +71,13 @@ TEST_F(SetupBigIntegersMultiplication, test_multiplication_big_int2_times_big_in
     EXPECT_TRUE(result_reverse.to_number_string() == big_int2_times_big_int3);
 }
 
+TEST_F(SetupBigIntegersMultiplication, test_multiplication_big_int4_times_big_int5) {
+    auto result = big_int4 * big_int5;
+    EXPECT_TRUE(result.to_number_string() == big_int4_times_big_int5);
+    auto result_reverse = big_int5 * big_int4;
+    EXPECT_TRUE(result_reverse.to_number_string() == big_int4_times_big_int5);
+}
+
 TEST_F(SetupBigIntegersMultiplication, test_multiplication_big_int1_times_big_int3_times_big_int2) {
     auto result = big_int1 * big_int2 * big_int3;
     EXPECT_TRUE(result.to_number_string() == big_int1_times_big_int2_times_big_int3);
@@ -72,6 +89,18 @@ TEST_F(SetupBigIntegersMultiplication, test_multiplication_big_int1_times_big_in
     EXPECT_TRUE(result_permuted3.to_number_string() == big_int1_times_big_int2_times_big_int3);
 }
 
+TEST_F(SetupBigIntegersMultiplication, test_multiplication_big_int1_times_big_int4_times_big_int2) {
+    auto result = big_int1 * big_int2 * big_int4;
+    EXPECT_TRUE(result.to_number_string() == big_int1_times_big_int2_times_big_int4);
+    auto result_permuted1 = big_int4 * big_int2 * big_int1;
+    EXPECT_TRUE(result_permuted1.to_number_string() == big_int1_times_big_int2_times_big_int4);
+    auto result_permuted2 = big_int2 * big_int1 * big_int4;
+    EXPECT_TRUE(result_permuted2.to_number_string() == big_int1_times_big_int2_times_big_int4);
+    auto result_permuted3 = big_int2 * big_int4 * big_int1;
+    EXPECT_TRUE(result_permuted3.to_number_string() == big_int1_times_big_int2_times_big_int4);
+}
+
+
 TEST_F(SetupBigIntegersMultiplication, test_multiplication_many_procucts) {
     long long int start{1176247};
     auto multiplicand = BigInteger<64>(start);
@@ -80,6 +109,17 @@ TEST_F(SetupBigIntegersMultiplication, test_multiplication_many_procucts) {
         auto multiplier = BigInteger<64>(string);
         auto product = multiplicand * multiplier;
         EXPECT_TRUE(product.to_number_string() == std::to_string(start * i));
+    }
+}
+
+TEST_F(SetupBigIntegersMultiplication, test_multiplication_many_negative_procucts) {
+    long long int start{1176247};
+    auto multiplicand = BigInteger<64>(start);
+    for (int i = 1; i < 1000; ++i) {
+        auto string = std::to_string(-i);
+        auto multiplier = BigInteger<64>(string);
+        auto product = multiplicand * multiplier;
+        EXPECT_TRUE(product.to_number_string() == std::to_string(-start * i));
     }
 }
 
