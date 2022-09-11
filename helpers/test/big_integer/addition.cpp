b4fb9541186e53d369b7f2278eed349c0cef2d49
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-09-11 10:03:35 +0200

    Unit test for addition of negative numbers.

--- helpers/test/big_integer/addition.cpp
+++ helpers/test/big_integer/addition.cpp
@@ -11,14 +11,18 @@ protected:
 	std::string big_number1{"9278118309180938109380983109"};
 	std::string big_number2{"2342349278113243248309180938234234109380983109572387399309213"};
 	std::string big_number3{"99985092384309223098482340923184482309480342341093809831372189479238438579"};
+    std::string big_number4{"-113243248327389789209180938234234109380983109572387399309213"};
 	std::string big_number1_plus_2{"2342349278113243248309180938234243387499292290510496780292322"};
 	std::string big_number2_plus_3{"99985092384311565447760454166432791490418576575203190814481761866637747792"};
+    std::string big_number1_plus_4{"-113243248327389789209180938234224831262673928634278018326104"};
+
 	std::string number1{"17"};
 	std::string number2{"9"};
 	std::string number1_plus_number2{"26"};
 	BigInteger<1000> big_int1 = BigInteger<1000>(big_number1);
 	BigInteger<1000> big_int2 = BigInteger<1000>(big_number2);
 	BigInteger<1000> big_int3 = BigInteger<1000>(big_number3);
+    BigInteger<1000> big_int4 = BigInteger<1000>(big_number4);
 
 };
 
@@ -32,6 +36,14 @@ TEST_F(SetupBigIntegersAddition, test_addition_big_number2_plus_big_number3)
 {
 	auto result = big_int2 + big_int3;
 	EXPECT_TRUE(result.to_number_string() == big_number2_plus_3);
+}
+
+TEST_F(SetupBigIntegersAddition, test_addition_big_number1_plus_big_number4)
+{
+    auto result = big_int1 + big_int4;
+    EXPECT_TRUE(result.to_number_string() == big_number1_plus_4);
+    result = big_int4 + big_int1;
+    EXPECT_TRUE(result.to_number_string() == big_number1_plus_4);
 }
 
 
